41dfdd19102b1b6d7e9784c456268a7a751446a0
AdriGarcia75 <adriancalagamba@gmail.com>
2025-04-22 16:51:13 +0200

    Change frontend port to 8080 and add QOL change to the Express init process in CLI

--- server/app.js
+++ server/app.js
@@ -8,12 +8,12 @@ const path = require('path');
 
 const attachmentsPath = path.join(__dirname, 'attachments');
 
+const port = 3000;
+
 if (!fs.existsSync(attachmentsPath)) {
     fs.mkdirSync(attachmentsPath, { recursive: true });
     console.log('La carpeta "attachments/" en "AnyTasks/server/" ha sido creada');
 }
-
-const port = 3000;
 
 app.listen(port, () => {
     console.log(`Servidor Express corriendo en http://localhost:${port}`)
@@ -30,4 +30,19 @@ app.get('/', (req, res) => {
 
 app.get('/data', (req, res) => {
     res.json({ message: 'Hola desde el backend de Express' })
+});
+
+process.on('SIGINT', () => {
+    console.log('\n¿Estás seguro de que quieres salir? (y/n)');
+
+    process.stdin.setEncoding('utf8');
+    process.stdin.once('data', function (data) {
+        const input = data.trim().toLowerCase();
+        if (input === 's' || input === 'y') {
+            console.log('Cerrando el servidor...');
+            process.exit(0);
+        } else {
+            console.log('Cancelado. El servidor sigue corriendo.');
+        }
+    });
 });
