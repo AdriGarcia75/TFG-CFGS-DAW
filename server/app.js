ed75f056c17df6d3ab26a598189a9bd8cb163401
AdriGarcia75 <adriancalagamba@gmail.com>
2025-04-23 16:00:20 +0200

    fixed project executables and changed CORS permits from all to only frontend

--- server/app.js
+++ server/app.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 const app = express();
 
-const port = 3000;
+const PORT = 3000;
+// this value must be the same used in client's npm start, if not, CORS will block any requests from the frontend
+const FRONTEND_PORT = 8080;
 
-const attachmentsPath = path.join(__dirname, 'attachments');
+const attachmentsPath = join(__dirname, 'attachments');
 
 // this checks if it exists an attachments folder (used to save all the files attached to any task)
-if (!fs.existsSync(attachmentsPath)) {
-    fs.mkdirSync(attachmentsPath, { recursive: true });
+if (!existsSync(attachmentsPath)) {
+    mkdirSync(attachmentsPath, { recursive: true });
     console.log('La carpeta "attachments/" en "./server/" ha sido creada');
 }
 
 app.listen(port, () => {
-    console.log(`Servidor Express corriendo en http://localhost:${port}`)
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`)
 });
 
 app.use(cors({
-    // cambiar luego cuando tengas front
-    origin: '*'
+    origin: FRONTEND_PORT
 }))
 
 app.get('/', (req, res) => {
